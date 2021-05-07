22b4762c5555bb38b95e25ef390685d5dddf4212
jamesRosierIBM <james.rosier@ibm.com>
2021-05-07 14:08:57 -0500

    initialize and add style

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Homepage from './homepage-component'
+import './homepage-styles.scss';
+
+function App() {
+  return (
+    <Homepage></Homepage>
+  );
+}
+
+export default App;
