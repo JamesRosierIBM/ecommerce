e1393bea4aba271dc2cbfccc84efd9da5b26b41c
jamesRosierIBM <james.rosier@ibm.com>
2021-05-07 15:42:21 -0500

    update menu item to navigate withRouter

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage-component.jsx'
 import './pages/homepage/homepage-styles.scss'
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Homepage></Homepage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+      </Switch>
+    </div>
   );
 }
 
