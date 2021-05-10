4c2ce9c0807ca1aae737e737a05b58cff6984b8f
jamesRosierIBM <james.rosier@ibm.com>
2021-05-10 08:56:22 -0500

    add header component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css'
 import Homepage from './pages/homepage/homepage-component.jsx'
 import ShopPage from './pages/shop/shop-component.jsx'
+import Header from './components/header/header-component.jsx'
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={ShopPage}/>
-
       </Switch>
     </div>
   );
