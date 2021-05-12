43b6b785d60a9f87ff5f3f2a96e4bc6e81f07349
jamesRosierIBM <james.rosier@ibm.com>
2021-05-12 12:22:29 -0500

    moved directory, shop data into its own reducer;created selectors and components for data flow

--- crwn-clothing/src/redux/store.js
+++ crwn-clothing/src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 //import the rootReducer that is all the reducers combined
 import rootReducer from './root-reducer';
 
 //this lets us easily add more middleware later
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store)
+
+export default {store, persistor};
