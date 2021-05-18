4e130629fffd46c834f6c52e486cdcd15b8a4812
jamesRosierIBM <james.rosier@ibm.com>
2021-05-18 14:37:35 -0500

    separating app into client folder, add express backend for Stripe

--- crwn-clothing/client/src/components/stripe-button/stripe-button.component.jsx
+++ crwn-clothing/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51IqfquBkXXUnZ1xnPxvOmnmhLqcMh2BFbCxCbAuTT5h5kkSMuyOpZfDKdf8u3QKEh8PP9vbVcFpWopeCR9lxTl3100CbDq4QBy';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      }
+    }).then(res => {
+      alert('Payment successful')
+    }).catch(err => {
+      console.log('Payment error:  ', JSON.parse(err));
+      alert(
+        'There was an issue with your payment. Please use the test credit card'
+      )
+    })
   };
 
   return (
