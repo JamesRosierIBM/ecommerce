9bd4729c6e0735684d7508106e219863a6c0cc22
jamesRosierIBM <james.rosier@ibm.com>
2021-05-20 14:43:35 -0500

    update to be PWA, add service worker, update manifest

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
 import CartProvider from "./providers/cart/cart.provider.jsx";
+import * as serviceWorker from './serviceWorker';
 
 import "./index.css";
 import App from "./App";
@@ -22,3 +23,5 @@ ReactDOM.render(
   </CartProvider>,
   document.getElementById("root")
 );
+//comment this out to stop the serviceworker
+serviceWorker.register();
